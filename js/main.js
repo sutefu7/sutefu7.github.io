6068da5eb244cb921e1332597c691fb03d9931fb
sutefu7 <mergework@gmail.com>
2019-08-09 11:08:23 +0900

    add for long vacation
    add for long vacation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 $(function() {
 
-    var items = ['エアコンの停止', '部屋の電気', '窓の鍵閉め', '玄関の鍵閉め'];
+    var zeroFormat = function(x) {
+        return x.toString().padStart(2, '0');
+    };
+
+    var dayOfWeek = function(x) {
+        switch (x) {
+            case 0: return "(日)"; break;
+            case 1: return "(月)"; break;
+            case 2: return "(火)"; break;
+            case 3: return "(水)"; break;
+            case 4: return "(木)"; break;
+            case 5: return "(金)"; break;
+            case 6: return "(土)"; break;
+        }
+    };
+
+    var items = ['エアコンの停止', '部屋の電気', '窓の鍵閉め', '玄関の鍵閉め', '電気のブレーカー', 'ガスの元栓', '水道の元栓'];
     for (var i = 0; i < items.length; i++) {
 
         // 項目名
@@ -30,11 +46,12 @@ $(function() {
         $('#chk' + index).change(function() {
             var now = new Date();
             var d = '' + now.getFullYear() + '/' +
-                (now.getMonth() + 1).toString().padStart(2, '0') + '/' +
-                now.getDate().toString().padStart(2, '0') + ' ' +
-                now.getHours().toString().padStart(2, '0') + ':' +
-                now.getMinutes().toString().padStart(2, '0') + ':' +
-                now.getSeconds().toString().padStart(2, '0');
+                zeroFormat((now.getMonth() + 1)) + '/' +
+                zeroFormat(now.getDate()) + ' ' +
+                dayOfWeek(now.getDay()) + ' ' +
+                zeroFormat(now.getHours()) + ':' +
+                zeroFormat(now.getMinutes()) + ':' +
+                zeroFormat(now.getSeconds());
 
             // コントロール名からナンバーのみ抽出して、対応するラベルにセット
             // （chk1 から 1 のような）
@@ -42,8 +59,8 @@ $(function() {
             $('#lbl' + index + 'b').text(d);
 
             // 切り替えるたびに、クッキーに保存
-            Cookies.set('chk' + index, $(this).prop('checked'));
-            Cookies.set('lbl' + index + 'b', d);
+            Cookies.set('chk' + index, $(this).prop('checked'), {expires: 30});
+            Cookies.set('lbl' + index + 'b', d, {expires: 30});
         });
         
     }
