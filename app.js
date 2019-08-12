e8c5a7a5b56059ef94c7bff3550b45c74c23f803
sutefu7 <mergework@gmail.com>
2019-08-12 17:13:35 +0900

    renew
    renew

--- app.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service_worker.js').then(function(x) {
-        console.log('Service worker registration successfull with scape: ', x.scope);
-    }).catch(function(err) {
-          console.log('Service worker registration faild: ', err);
-    });
-}
-
